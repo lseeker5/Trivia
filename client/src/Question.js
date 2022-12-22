5329dd7a1cbb2a726df30e3d59353d3d629cd815
lseeker5 <118029799+lseeker5@users.noreply.github.com>
2022-12-23 06:59:45 +0800

    finally solved the bug yayyy

--- client/src/Question.js
+++ client/src/Question.js
@@ -2,21 +2,23 @@ import './Question.css'
 import React from 'react'
 
 export default function Question(props){
-    let options=[...props.question.incorrectAnswers]
-   
+    let options=[...props.question.incorrectAnswers]  
     let index=Math.round(3*Math.random())
-    
-    let fullOptions=options.splice(index,0,props.question.correctAnswer)
+    let correctAnswer=props.question.correctAnswer
+    options.splice(index,0,correctAnswer)
    
-    console.log(fullOptions)
+    
     
     return(
-        <div>
-            <p>test</p>
-            <p>{fullOptions[0]}</p>
-            <p>{fullOptions[1]}</p>
-            <p>{fullOptions[2]}</p>
-            <p>{fullOptions[3]}</p>
+        <div className='overall-question'>
+            <h1>{props.question.question}</h1>
+            <div className='options'>
+                <p className='option first'>{options[0]}</p>
+                <p className='option second'>{options[1]}</p>
+                <p className='option third'>{options[2]}</p>
+                <p className='option fourth'>{options[3]}</p>
+            </div>
+            
         </div>
     )
 }
